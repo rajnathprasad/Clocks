7c3f009529e4ea398214aeea95691b2ea4a95de7
rajnathprasad <rajnathprasad.cs@gmail.com>
2025-01-19 02:33:18 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Simple Analog and digital clocks!
---
+++ script.js
@@ -0,0 +1,25 @@
+function displayAnalogTime(){
+    let date = new Date();
+    let hh=date.getHours();
+    let mm=date.getMinutes();
+    let ss=date.getSeconds();
+    let hr = document.querySelector("#hour");
+    let min = document.querySelector("#minute");
+    let sec = document.querySelector("#second");
+    let hhRotation =30*hh + mm/2;
+    let mmRotation = 6*mm;
+    let ssRotation = 6*ss;
+    hr.style.transform = `rotate(${hhRotation}deg)`;
+    min.style.transform = `rotate(${mmRotation}deg)`;
+    sec.style.transform = `rotate(${ssRotation}deg)`;
+}
+function displayDigitalTime(){
+    let date = new Date();
+    let hh=date.getHours();
+    let mm=date.getMinutes();
+    let ss=date.getSeconds();
+    let digiClock = document.querySelector(".digiClock");
+    digiClock.innerText=`${hh}  :  ${mm}  :  ${ss}`;
+}
+setInterval(displayAnalogTime,1000);
+setInterval(displayDigitalTime,1000);
